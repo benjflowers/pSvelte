86c6c406589bcd6b4981034d2583019fa7a144cd
benjflowers <benjaminjflowers@gmail.com>
2020-03-31 17:57:54 -0500

    fixed line sketch

--- src/sketchbook/lines.js
+++ src/sketchbook/lines.js
@@ -2,13 +2,14 @@ let sketch = p5 => {
   var t = .01
   p5.setup = () => {
     p5.createCanvas(300, 300);
-    p5.background(60, 40, 200);
+    p5.background(60, 40, 800);
   }
 
   p5.draw = () => {
-    p5.stroke(`rgb(100, ${p5.noise(t) * 180}, 50)`)
+    let stroke = p5.noise(t) * 120
+    p5.stroke(100,stroke,20);
     p5.translate(p5.width/2, p5.height/2)
-    p5.line(0, 0, p5.cos(t) * 60, p5.sin(t) * 60);
+    p5.line(0, 0, p5.cos(t) * 80, p5.sin(t) * 80);
 
     t += .01;
   }

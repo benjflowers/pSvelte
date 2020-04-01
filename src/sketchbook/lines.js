394fb4f7e336fb9f883363c54758e6ead639c4a1
benjflowers <benjaminjflowers@gmail.com>
2020-04-01 18:37:24 -0500

    finished sharp lines

--- src/sketchbook/lines.js
+++ src/sketchbook/lines.js
@@ -1,36 +1,39 @@
 let sketch = p5 => {
 
-  var inc = .01;
-  var scl = 100;
-  var rows;// = p5.floor(p5.height*scl);
-  var cols;// = p5.floor(p5.width*scl);
+  var scl = 10;
+  var rows;
+  var cols;
   var offset;
   var offset;
   p5.setup = () => {
-    p5.createCanvas(400, 400);
+    p5.createCanvas(300, 300);
     p5.background(200, 200, 200);
     p5.strokeWeight(3);
 
     rows = p5.floor(p5.height/scl);
     cols = p5.floor(p5.width/scl);
     offset = p5.ceil(scl/2);
-
-    p5.noLoop();
   }
 
   p5.draw = () => {
+    p5.background(200, 200, 200);
     p5.noFill();
 
-    for(let y = 0; y < cols; y++) {
+    for(let y = 0; y < rows; y++) {
       p5.beginShape();
 
-      for(let x = 0; x < rows; x++) {
+      for(let x = 0; x < cols; x++) {
+        let add = 0;
+        if(x == 0 || x == rows - 1) {
+          add = 0;
+        } else {
+          add = p5.random(10);
+        }
 
-        p5.vertex(x*scl + offset, y*scl + offset);
+        p5.vertex(x*scl + offset, y*scl + offset + add);
       }
       p5.endShape();
     }
-    inc += .01;
   }
 }
 
